061ab08c9233d4ea5fdf339946c3a370897cc5c0
msuemmer <moritz.suemmermann@genedata.com>
2022-02-24 19:27:37 +0100

    change

--- src/main/java/total/Project.java
+++ src/main/java/total/Project.java
@@ -1,6 +1,20 @@
 package total;
 
-import java.util.List;
+import java.util.*;
 
 public record Project(String name, int duration, int score, int bestBefore, List<SkillLevel> requiredLevels) {
+
+    public Optional<Set<Contributor>> needsTheseContributors(List<Contributor> availableContributors) {
+        Set<Contributor> required = new HashSet<>();
+        for (SkillLevel requiredLevel : requiredLevels) {
+            Optional<Contributor> optionalContributor = availableContributors
+                    .stream()
+                    .filter(contributor -> !required.contains(contributor))
+                    .filter(contributor -> contributor.skillLevel().getOrDefault(requiredLevel.skill(), 0) > requiredLevel.level())
+                    .min(Comparator.comparing(contributor -> contributor.skillLevel().getOrDefault(requiredLevel.skill(), 0)));
+            if (optionalContributor.isEmpty()) return Optional.empty();
+            else required.add(optionalContributor.get());
+        }
+        return Optional.of(required);
+    }
 }
--- src/main/java/total/solvers/SimpleSolver.java
+++ src/main/java/total/solvers/SimpleSolver.java
@@ -1,6 +1,11 @@
 package total.solvers;
 
 import total.ProblemDescription;
+import total.Project;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SimpleSolver extends Solver {
     public SimpleSolver(ProblemDescription problem) {
@@ -9,6 +14,14 @@ public class SimpleSolver extends Solver {
 
     @Override
     public void solve() {
-
+        final List<Project> projectsByScore = problem.projects()
+                .stream()
+                .sorted(Comparator.comparing(Project::score).reversed())
+                .collect(Collectors.toList());
+        for (Project project : projectsByScore) {
+            project.needsTheseContributors(problem.contributors());
+        }
     }
+
+
 }
